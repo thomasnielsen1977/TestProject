3141a7228f31c82c076178057894d4b4c5397a7f
Thomas Nielsen <thomasnielsen1977@gmail.com>
2015-11-27 22:48:43 +0100

    Add files

---
+++ code/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class Test
+    {
+        public static string RunTest()
+        {
+            return "KLOKKEN ER " + DateTime.Now.ToString();
+        }
+    }
+}
